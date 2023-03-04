0a81d4db4087d2816795072254d3d5dbb7f2cf9d
JacekGos <jacekg93@gmail.com>
2023-03-04 15:59:04 +0100

    TFM-3 initilized spring boot project for api-gateway

---
+++ api-gateway/src/test/java/com/jacekg/teamfinder/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacekg.teamfinder.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
