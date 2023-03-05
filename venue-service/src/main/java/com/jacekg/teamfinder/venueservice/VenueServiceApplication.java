65ee1d81ea69243e190495e32d865102546ac9c7
JacekGos <jacekg93@gmail.com>
2023-03-05 15:30:29 +0100

    Setup application.properties for services to communicate with discovery-server

--- venue-service/src/main/java/com/jacekg/teamfinder/venueservice/VenueServiceApplication.java
+++ venue-service/src/main/java/com/jacekg/teamfinder/venueservice/VenueServiceApplication.java
@@ -2,8 +2,10 @@ package com.jacekg.teamfinder.venueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VenueServiceApplication {
 
 	public static void main(String[] args) {
