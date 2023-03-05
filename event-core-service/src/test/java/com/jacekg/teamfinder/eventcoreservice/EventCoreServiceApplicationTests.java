5ded880e8dda17558c99450a0642c8690e1292bd
JacekGos <jacekg93@gmail.com>
2023-03-05 10:48:39 +0100

    TFM-5 Initalized event-core-service spring boot project

---
+++ event-core-service/src/test/java/com/jacekg/teamfinder/eventcoreservice/EventCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacekg.teamfinder.eventcoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
