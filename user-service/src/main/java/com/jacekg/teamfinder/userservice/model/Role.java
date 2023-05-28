163e1010bae43b3794bbd021c27d3d018ebed23d
JacekGos <jacekg93@gmail.com>
2023-05-28 18:33:53 +0200

    Created base model for user. Added V1 sql script

---
+++ user-service/src/main/java/com/jacekg/teamfinder/userservice/model/Role.java
@@ -0,0 +1,31 @@
+package com.jacekg.teamfinder.userservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+}
---
+++ user-service/src/main/java/com/jacekg/teamfinder/userservice/model/UserEvent.java
@@ -0,0 +1,35 @@
+package com.jacekg.teamfinder.userservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "user_event")
+public class UserEvent {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+	
+	@Column(name = "user_id")
+	private Long userId;
+	
+	@Column(name = "event_id")
+	private Long eventId;
+
+	public UserEvent(Long userId, Long eventId) {
+		this.userId = userId;
+		this.eventId = eventId;
+	}
+}
