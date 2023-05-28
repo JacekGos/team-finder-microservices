5b5d43ab57c503c3c9c762e67c3a8394d992db73
JacekGos <jacekg93@gmail.com>
2023-05-28 18:44:44 +0200

    Added repository, service, controller layers

--- event-serivce/src/main/java/com/jacekg/teamfinder/eventservice/event/service/EventServiceImpl.java
+++ event-serivce/src/main/java/com/jacekg/teamfinder/eventservice/event/service/EventServiceImpl.java
@@ -1,18 +1,12 @@
 package com.jacekg.teamfinder.eventservice.event.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import com.jacekg.teamfinder.eventservice.event.model.BasketballEvent;
-import com.jacekg.teamfinder.eventservice.event.model.BicycleEvent;
 import com.jacekg.teamfinder.eventservice.event.model.Event;
-import com.jacekg.teamfinder.eventservice.event.model.FootballEvent;
-import com.jacekg.teamfinder.eventservice.event.model.TennisEvent;
-import com.jacekg.teamfinder.eventservice.event.model.VolleyballEvent;
 import com.jacekg.teamfinder.eventservice.event.repository.EventRepository;
 
 import lombok.AllArgsConstructor;
