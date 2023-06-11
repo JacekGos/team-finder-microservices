f1950b1e81741f2d627231767403548c1dc7e36e
JacekGos <jacekg93@gmail.com>
2023-06-11 16:08:12 +0200

    TFM-6 Updated notification-service project structure

---
+++ notification-service/src/test/java/com/jacekg/teamfinder/notificationservice/NotificationServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jacekg.teamfinder.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
